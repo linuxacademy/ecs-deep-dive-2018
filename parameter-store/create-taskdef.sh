ea0a6ea51ea5a8a01c99baf5fc351016a279c9ea
Mark Richman <mark@markrichman.com>
2018-10-26 12:17:18 -0400

    Removed region

--- parameter-store/create-taskdef.sh
+++ parameter-store/create-taskdef.sh
@@ -2,10 +2,8 @@
 
 ACCOUNT_ID=<Your_AWS_account_ID_here>
 S3_URI=<S3_URI_to_access-tesh.sh>
-AWS_REGION=us-east-1
 
 aws ecs register-task-definition \
   --family access-test \
   --task-role-arn "arn:aws:iam::${ACCOUNT_ID}:role/prod-app1" \
   --container-definitions name="access-test",image="nginx",portMappings="[{containerPort=80,hostPort=80,protocol=tcp}]",readonlyRootFilesystem=false,cpu=512,memory=490,essential=true,entryPoint="sh,-c",command="\"/bin/sh -c \\\"apt-get update ; apt-get -y install curl ; curl -O $S3_URI ; chmod +x access-test.sh ; ./access-test.sh ; nginx -g 'daemon off;'\\\"\"" \
-  --region $AWS_REGION
